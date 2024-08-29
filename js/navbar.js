d937daef95045be3d363e91e7851dfc88cc5516e
Ana Zambrana <anazambrana668@gmail.com>
2024-08-29 03:51:15 -0300

    Se creo un nuevo archivo en la carpeta scss _dropdown.scss,  nuevas modificaciones en botones y arreglos en responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
-const hamburger = document.querySelector(".menu-responsive");
-const navBar = document.querySelector(".nav__container");
+const toggle_btn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown__menu')
 
-hamburger.onclick = function() {
-    navBar.classList.toggle("active");
+toggle_btn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.className = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
 }
